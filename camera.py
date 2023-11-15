dbe92d9690509f1213d77e65b56144ca42d93850
meekamunz <meekamunz@gmail.com>
2023-11-15 21:43:34 +0000

    added a module for creating a video

--- camera.py
+++ camera.py
@@ -1,5 +1,20 @@
 from picamera2 import Picamera2
-from libcamera import controls
+from picamera2.encoders import H264Encoder
+#from libcamera import controls
+from datetime import datetime
+import time
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+encoder = H264Encoder(10000000)
+
+def time_now():
+    datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 def get_video(output_file, format, duration):
     #libcamera-vid -t duration -o output_file.format
+    #libcamera-vid -t 20000 -o  time_now().mp4
+    picam2.start_recording(encoder, f'{output_file}.{format}')
+    time.sleep(duration)
+    picam2.stop_recording()
