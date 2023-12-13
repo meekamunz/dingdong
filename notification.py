6ace47f1a2f813ff183f2936b89834716beccde7
meekamunz <meekamunz@gmail.com>
2023-12-13 22:31:11 +0000

    Created post for image upload request.  Returns image upload url

--- notification.py
+++ notification.py
@@ -14,21 +14,36 @@
 
 # curl --header 'Access-Token: o.K8sYAATHDyYjX7eURSn0g6z5wSnbnkXD' https://api.pushbullet.com/v2/pushes -d type=note&title="dingdong-test"&channel_tag="hawthorne-doorbell"
 
+#curl --header 'Access-Token: <your_access_token_here>' \
+#     --header 'Content-Type: application/json' \
+#     --data-binary '{"file_name":"cat.jpg","file_type":"image/jpeg"}' \
+#     --request POST \
+#
+#     https://api.pushbullet.com/v2/upload-request
 
 
-import requests
-import json
 
-ACCESS_TOKEN = 'o.K8sYAATHDyYjX7eURSn0g6z5wSnbnkXD'
-resp = requests.post('https://api.pushbullet.com/v2/upload-request', data=json.dumps({'file_name': 'image.jpg'}), headers={'Authorization': 'Bearer ' + ACCESS_TOKEN, 'Content-Type': 'application/json'})
-if resp.status_code != 200:
-    raise Exception('failed to request upload')
-r = resp.json()
-resp = requests.post(r['upload_url'], data=r['data'], files={'file': open('image.jpg', 'rb')})
-if resp.status_code != 204:
-    raise Exception('failed to upload file')
-print r['file_name'], r['file_type'], r['file_url']
+import requests, json
 
-
+# Test Config
+access_token = 'o.K8sYAATHDyYjX7eURSn0g6z5wSnbnkXD'
+image = 2023-12-13_21-56-16.jpg
 
 # It's actually a 3 step process. You send a file upload request to pushbullet, they respond with an amazon URL, you send the file to amazon, and then send a pushbullet notification to a device.
+
+POST https://api.pushbullet.com/v2/upload-request
+
+
+# Request file upload, returns a URL to upload the file to
+def request_image_upload_url(access_token, image):    
+    resp = requests.post('https://api.pushbullet.com/v2/upload-request', data=json.dumps({'file_name': image, 'file_type': 'image/jpeg'}), headers={'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'})
+    if resp.status_code != 200:
+        raise Exception('failed to request upload')
+    r = resp.json()
+    resp = requests.post(r['upload_url'], data=r['data'], files={'file': open(image, 'rb')})
+    if resp.status_code != 204:
+        raise Exception('failed to upload file')
+    print(r['file_name'], r['file_type'], r['file_url'])
+    return(r['file_url'])
+
+# Send Pushbullet notification to hawthorne-doorbell channel_tag
