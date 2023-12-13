880a0c1da8c66c8e343f48067841d62644ee45d5
meekamunz <meekamunz@gmail.com>
2023-12-13 22:57:10 +0000

    moved time_now to fuctions.py from camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,6 @@
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, Quality
 from picamera2.outputs import FfmpegOutput
-from datetime import datetime
 import time
 
 # picam settings
@@ -11,12 +10,6 @@ picam2.configure(video_config)
 encoder = H264Encoder(10000000)
 output = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 stream.m3u8")
 #output = FfmpegOutput("-f dash -window_size 5 -use_template 1 -use_timeline 1 stream.mpd")
-
-# time
-def time_now():
-    i = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    return i
-
 
 # record video for x time
 def rec_video(output_file, format, duration):
